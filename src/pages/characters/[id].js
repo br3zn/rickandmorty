a2d9ab569ffc49b3ad545f19c1453333a6539a4d
brezn <br3zn@users.noreply.github.com>
2022-01-20 21:57:57 +0100

    nextjs static site generation for testing

---
+++ src/pages/characters/[id].js
@@ -0,0 +1,26 @@
+export default function Character({ character }) {}
+
+export async function getStaticPaths() {
+  const { data } = await client.query({
+    query: gql`
+      query Characters {
+        characters {
+          results {
+            id
+            name
+            species
+            status
+            image
+          }
+        }
+      }
+    `,
+  });
+
+  const paths = data.characters.results.map((character) => ({
+    params: { id: character.id },
+  }));
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {}
