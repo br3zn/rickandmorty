58a139f49293978a71b224c9f5adc54fdf85603f
brezn <br3zn@users.noreply.github.com>
2022-01-23 18:12:32 +0100

    Add unstyled history back button for detail view

--- src/pages/character/[id].js
+++ src/pages/character/[id].js
@@ -3,10 +3,18 @@ import { CharacterDetails } from "../../components/CharacterDetails";
 
 export default function DisplayCharacterDetails() {
   const { query } = useRouter();
+  const router = useRouter();
   const charId = Number.parseInt(query.id);
   return (
     <>
-      <p className="mt-16 text-2xl text-center">Character {charId}</p>
+      <button
+        className="mt-4 text-blue-600"
+        type="button"
+        onClick={() => router.back()}
+      >
+        Back to overview
+      </button>
+      <p className="mt-16 text-2xl text-center">Character #{charId}</p>
       <CharacterDetails charId={charId} />
     </>
   );
