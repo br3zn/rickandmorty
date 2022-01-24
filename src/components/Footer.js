fa1da66e891bb4f7e8ccc546a06149fa142427fd
brezn <br3zn@users.noreply.github.com>
2022-01-24 02:47:56 +0100

    Set correct z-index for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="sticky bottom-0 left-0 w-full bg-opacity-40 backdrop-blur-sm bg-gray-400 flex flex-1 py-4 border-t border-solid border-gray-400 justify-center items-center">
+    <footer className="z-30 sticky bottom-0 left-0 w-full bg-opacity-40 backdrop-blur-sm bg-gray-400 flex flex-1 py-4 border-t border-solid border-gray-400 justify-center items-center">
       <a
         href="https://github.com/br3zn"
         target="_blank"
